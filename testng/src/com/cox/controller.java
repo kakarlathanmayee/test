927951bf2fec7e79f90947eec0ff15d8fc52ffab
thanmayee <thanmayeekj@gmail.com>
2020-06-05 15:48:05 +0530

    second commit

---
+++ testng/src/com/cox/controller.java
@@ -0,0 +1,19 @@
+package com.cox;
+
+import java.util.Set;
+import com.cox.gmail;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class controller {
+public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\BALAKUMAR MK\\Downloads\\chromedriver_win32\\chromedriver.exe"); 
+		 WebDriver driver = new ChromeDriver();
+//		driver.navigate().to("http://google.com");
+//		String appTitle = driver.getTitle();
+//		System.out.println("Application title is :: "+appTitle);
+//		driver.quit();
+	//	 gmail.gmailLogin(driver);
+	}
+}
